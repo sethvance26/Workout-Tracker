8fd624df17439c4ba4a2aeb31a1f27b337e8d345
Seth <sethvance26@gmail.com>
2021-06-29 02:59:21 -0400

    editing mongoose connection to test routes

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
-
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
