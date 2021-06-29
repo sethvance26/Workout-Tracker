0b97f161e0b49c1580e532a925be58d40924361c
Seth <sethvance26@gmail.com>
2021-06-29 17:25:43 -0400

    adding to workout schema

--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,39 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+
+
 const workoutSchema = new Schema ({
     exercises: [{
+        
         name: {
             type: String,
             required: ""
-        }
+        },
+
+        type: {
+            type: String,
+            required: ""
+        },
+        weight: {
+
+        },
+        sets: {
+
+        },
+        reps: {
+
+        },
+        duration: {
+            type: Number,
+            required: ""
+        },
+        distance: {
+            type: Number,
+        },
     }]
-})
+});
+
+const workoutSchema = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Workout;
