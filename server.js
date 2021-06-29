baf534bf44e1df00b1c6065670f24deef9a774b1
Seth <sethvance26@gmail.com>
2021-06-28 20:59:51 -0400

    installing dependencies, changing db name

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
 
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
