f003c59d59d4bbd2681e11e74dce9c2c5aadaa46
Seth <sethvance26@gmail.com>
2021-10-16 18:40:54 -0400

    reconnecting database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
